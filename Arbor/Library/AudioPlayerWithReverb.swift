8c619802af6f8393a0d1f619c49b3bc595a18289
Armaan Aggarwal <armaanaggarwal@gmail.com>
2026-05-17 19:53:32 -0700

    Reduce overlap int from 32 to 16

--- Arbor/Library/AudioPlayerWithReverb.swift
+++ Arbor/Library/AudioPlayerWithReverb.swift
@@ -87,7 +87,8 @@ final class AudioPlayerWithReverb {
                 
         // Default parameters
         reverbNode.wetDryMix = effects.reverbMix
-        pitchNode.overlap = 32.0 // to increase quality of speed changes. see https://developer.apple.com/documentation/avfaudio/avaudiounittimepitch/overlap
+        pitchNode.overlap = 16.0 // to increase quality of speed changes. see https://developer.apple.com/documentation/avfaudio/avaudiounittimepitch/overlap
+        
         pitchNode.rate = effects.speedRate
         
         // Connect nodes: player -> pitch -> reverb -> output
